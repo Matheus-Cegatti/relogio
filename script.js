a03f6cf02e67a7dc8a8c506adf38b077958159e9
Matheus-Cegatti <cegattimatheus@gmail.com>
2022-10-13 13:41:09 -0300

    colocando a função no setInterval, fazendo o relogio funcionar

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ const definindoRelogio = () => {
     const dataAtual = new Date();
 
     const segundosPorcentagem = dataAtual.getSeconds() / 60;
-    const minutosPorcentagem = dataAtual.getMinutes() / 60;
-    const horasPorcentagem = dataAtual.getHours() / 12;
+    const minutosPorcentagem = (segundosPorcentagem + dataAtual.getMinutes()) / 60;
+    const horasPorcentagem = (minutosPorcentagem + dataAtual.getHours()) / 12;
 
     rotacaoDoPonteiro(ponteiroSegundos, segundosPorcentagem);
     rotacaoDoPonteiro(ponteiroMinutos, minutosPorcentagem);
@@ -19,3 +19,6 @@ const definindoRelogio = () => {
 
 }
 
+definindoRelogio();
+
+setInterval(definindoRelogio, 1000);
